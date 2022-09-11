c9158d108c14ebb34f5918f2f1cbaa0905c1d5d6
KeiGarcia <keilataniagarcia3@gmail.com>
2022-09-10 23:06:54 -0300

    componente menu

---
+++ wiselink/src/componet/menu/menu.js
@@ -0,0 +1,26 @@
+import "./menu.css"
+
+
+import React from 'react'
+
+export const menu = () => {
+  return (
+
+    <>
+
+    <header>
+        <nav>
+            <ul>
+                <li> <a>Home</a>  </li>
+                <li> <a>About me</a>  </li>
+                <li> <a>Contact</a>  </li>
+                <li> <a>Features</a>  </li>
+            </ul>
+        </nav>
+    </header>
+
+    </>
+  )
+}
+
+export default menu
